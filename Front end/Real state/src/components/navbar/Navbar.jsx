4458af39108e1756452246588c7a262e1634c071
AHShourav <ashourav@my.centennialcollege.ca>
2024-11-29 22:55:47 -0500

    Front end

---
+++ Front end/Real state/src/components/navbar/Navbar.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const user = true;
+
+  return (
+    <nav>
+      <div className="left">
+        <Link to="/" className="logo">
+          <img src="/logo.jpg" alt="HomeHaven Logo" />
+          <span>HomeHaven</span>
+        </Link>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/agents">Agents</Link>
+        <Link to="/list">Listings</Link>
+      </div>
+      <div className="right">
+        {user ? (
+          <div className="user">
+            <img src="/user.jpg" alt="User Avatar" />
+            <span>HomeHaven User</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <Link to="/login">Sign in</Link>
+            <Link to="/register" className="register">
+              Sign up
+            </Link>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt="Menu Icon"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/agents">Agents</Link>
+          <Link to="/list">Listings</Link>
+          <Link to="/login">Sign in</Link>
+          <Link to="/register">Sign up</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
