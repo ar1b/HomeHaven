488ddaaaccee1e46b5c4978b50b078351f95394e
Jethro Cao <jethrocao@hotmail.com>
2024-12-01 18:08:26 -0500

    User update with form works

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
+import UserUpdate from './pages/UserUpdate';
 
 const App = () => {
   return (
     <div>
       Hello World
-      <SignIn />
+      <UserUpdate />
     </div>
   )
 }
