e0fd9ee6b75da6804b60a9b958427dfe89dce2d3
ar1b <arib.kamal99@gmail.com>
2024-12-01 13:56:02 -0500

    added proxy, created fetch requests in my frontend (not working)

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const SignUp = () => {
+
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value});
+    console.log(formData)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
-    <div>
-      Sign Up
+    <div className="pt-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input type='text' placeholder='Username' className='border p-3 rounded-lg' id="username"  onChange={handleChange}/>
+        <input type='text' placeholder='E-mail' className='border p-3 rounded-lg' id="email" onChange={handleChange}/>
+        <input type='password' placeholder='Password' className='border p-3 rounded-lg' id="password" onChange={handleChange}/>
+
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
+      </form>
+      <div className="flex gap-2 m-5">
+        <p>Have an account?</p>
+        <span className="text-blue-700">Sign in</span>
+      </div>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/userRoutes.js"
+import authRouter from "./routes/authRoute.js"
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -10,7 +12,22 @@ mongoose.connect(process.env.MONGO).then(() => {
 })
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000")
+})
+
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter );
+
+//Middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    });
 })
