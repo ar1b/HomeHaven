1181bfcf08506865524609a372fb19c22970b755
Jethro Cao <jethrocao@hotmail.com>
2024-12-01 19:37:48 -0500

    search page
    no pictures returned
    owner user id returned not name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import UserUpdate from './pages/UserUpdate';
+import ListingSearch from './pages/ListingSearch';
 
 const App = () => {
   return (
     <div>
       Hello World
-      <UserUpdate />
+      <ListingSearch />
     </div>
   )
 }
---
+++ client/src/pages/ListingSearch.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import {useState} from 'react';
+import './listingSearch.css';
+
+    function ListingSearch(){
+
+    const [listingList, setListingList] = useState();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        //searchstring is all lowercase
+        const searchstring = document.getElementById("searchBar").value;
+        let fetchURL = "/api-listings/search"
+        if(searchstring){
+            fetchURL = fetchURL + "?searchstring=" + searchstring;
+        }
+
+        await fetch(fetchURL)
+            .then( async res => { 
+                const obj = await res.json();
+                console.log(obj[0]);
+                setListingList(obj);
+            })
+            .catch( err => {console.log("Error! " + err)} );
+    }
+    
+    function ListingBoxes(){
+        return(
+            <div>
+                {listingList.map( (listing) => (
+                    <div className="listingBox">
+                        <p>Owner ID: {listing.owner}</p>
+                        <p>Address: {listing.address}</p>
+                        <p>Type: {listing.type}</p>
+                        <p>Price: {listing.price}</p>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+/*
+    function ListingBox(){
+        return(
+
+        );
+    }
+*/
+    return (
+        <div className="mainPart">
+            <h2>Search Page</h2>
+            <div>
+                <label htmlFor="searchBar">Search:</label>
+                <input id="searchBar" name="searchBar" type="text" placeholder="Enter Search" />
+                <br/>
+                <button id="submitBtn" onClick={handleSubmit}>Search</button>
+                <br/>
+                <br/>
+
+            </div>
+            <ListingBoxes />
+        </div>
+    );
+}
+
+export default ListingSearch;
