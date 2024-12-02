505f8066be6c9ced6a8620eeae7a68e2b8caf3fd
Jethro Cao <jethrocao@hotmail.com>
2024-12-01 22:54:47 -0500

    asdf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
       <BrowserRouter>
       <Header />
       <Routes>
-          <Route index element={<SignIn />} />
+          <Route index element={<ListingSearch />} />
           <Route path='/signup' element={<SignUp />}/>
           <Route path='/signin' element={<SignIn />}/>
           <Route path='/userupdate' element={<UserUpdate />}/>
