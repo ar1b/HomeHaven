2eb1ee3b1ff63c2ab9395b7ba2e3d7bf4de1c44d
Jethro Cao <jethrocao@hotmail.com>
2024-12-12 17:08:40 -0500

    commented out CORS

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,11 @@ const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/userRouter.js');
 const listingRouter = require('./routes/listingRouter.js');
 
-const cors = require('cors');
+//const cors = require('cors');
+//app.use(cors({origin: '*'}));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: '*'}));
 
 mongoose.connect(process.env.DB_CONNECTION_STRING, { useNewUrlParser: true });
 const db = mongoose.connection;
