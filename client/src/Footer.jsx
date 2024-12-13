ec09db2846ac1cc6fba6d320cf97c03cdd5ac3a5
krishishah296 <krishishah97@gmail.com>
2024-12-12 19:47:01 -0500

    footer

---
+++ client/src/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="bg-gray-100 py-5 text-center border-t border-gray-300 text-gray-800">
+            <p>© 2024 HomeHaven. All rights reserved.</p>
+            <p>
+                Built with by{" "}
+                <a
+                    href="add GitHub link for our repository"
+                    className="text-blue-500 hover:underline"
+                >
+                    HomeHaven
+                </a>
+            </p>
+        </footer>
+    );
+};
+
+export default Footer;
