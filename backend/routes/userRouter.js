ae0c3b8220f8c93785836eb7841f454c5ceaf2fa
Jethro Cao <jethrocao@hotmail.com>
2024-12-13 14:04:37 -0500

    backend 1

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -18,7 +18,7 @@ router.post('/create', async (req, res) =>{
             });
             const savedUser = await user.save();
             const token = jwt.sign({id: user._id, email: user.email}, process.env.JWT_SECRET, {expiresIn: '30d'});
-            res.cookie('token', token, {maxAge: 30*24*60*60*1000});
+            res.cookie('token', token, {maxAge: 30*24*60*60*1000, sameSite: false});
             res.json({message: 'created user', token});
         }
         else{
@@ -76,7 +76,7 @@ router.patch('/update', async (req, res) => {
             await searchResult.save();
 
             const newToken = jwt.sign({id: searchResult._id, email: searchResult.email}, process.env.JWT_SECRET, {expiresIn: '30d'});
-            res.cookie('token', newToken, {maxAge: 30*24*60*60*1000});
+            res.cookie('token', newToken, {maxAge: 30*24*60*60*1000, sameSite: false});
             res.json({message: 'updated user ' + searchResult.email, newToken});
         }
         catch(err){
@@ -97,7 +97,7 @@ router.post('/login', async (req, res) =>{
             const isPasswordValid = await bcrypt.compare(req.body.password, searchResult.password);
             if(isPasswordValid){
                 const token = jwt.sign({id: searchResult._id, email: searchResult.email}, process.env.JWT_SECRET, {expiresIn: '30d'});
-                res.cookie('token', token, {maxAge: 30*24*60*60*1000});
+                res.cookie('token', token, {maxAge: 30*24*60*60*1000, sameSite: false});
                 res.json({message: 'AUTHORIZED FOR ' + req.body.email});
             }
             else{
